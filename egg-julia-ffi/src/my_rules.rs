2a3e2671f8abe40a2dafa1efb7c73130117bd382
ParthsarthiSingh-glang <pssclass12th@gmail.com>
2026-06-22 18:03:47 +0530

    my_rules reduntant

--- egg-julia-ffi/src/my_rules.rs
+++ egg-julia-ffi/src/my_rules.rs
@@ -1,26 +1,38 @@
-// my_rules.rs
+//
+//
+//
+//
+//
+//                             REDUNTANT FOR NOW 
+//               WE WANT THE THE RULES TO BE A LARGE EXPRESSIONS GENERATOR , NOT A SIMPLIFIER . 
+//          JOB OF GETTING THE OPTIMIZED ALTERNATIVES IS DONE BY NUMERICAL METHODS INSTEAD OF REWRITE RULES .
+//
+//
 
 
-use crate::{ConstantFold, MathLang};
-use egg::{rewrite, Rewrite};
+// // my_rules.rs
 
-pub fn my_rules() -> Vec<Rewrite<MathLang, ConstantFold>> {
-    vec![
-        rewrite!("flip--";
-            "(- (sqrt ?a) (sqrt ?b))" =>
-            "(/ (- ?a ?b) (+ (sqrt ?a) (sqrt ?b)))"),
 
-        rewrite!("associate--l+b"; "(- (+ ?a ?b) ?c)" => "(+ ?b (- ?a ?c))"),
-        rewrite!("associate--r+"; "(- ?a (+ ?b ?c))" => "(- (- ?a ?b) ?c)"),
+// use crate::{ConstantFold, MathLang};
+// use egg::{rewrite, Rewrite};
 
-        rewrite!("cancel-+-";  "(- (+ ?a ?b) ?a)" => "?b"),
-        rewrite!("cancel-+-2"; "(- (+ ?a ?b) ?b)" => "?a"),
+// pub fn my_rules() -> Vec<Rewrite<MathLang, ConstantFold>> {
+//     vec![
+//         rewrite!("flip--";
+//             "(- (sqrt ?a) (sqrt ?b))" =>
+//             "(/ (- ?a ?b) (+ (sqrt ?a) (sqrt ?b)))"),
 
-        rewrite!("cancel--+";  "(- ?a (+ ?a ?b))" => "(neg ?b)"),
-        rewrite!("cancel--+2"; "(- ?a (+ ?b ?a))" => "(neg ?b)"),
+//         rewrite!("associate--l+b"; "(- (+ ?a ?b) ?c)" => "(+ ?b (- ?a ?c))"),
+//         rewrite!("associate--r+"; "(- ?a (+ ?b ?c))" => "(- (- ?a ?b) ?c)"),
 
-        rewrite!("+-inverses";     "(- ?a ?a)" => "0"),
-        rewrite!("+-rgt-identity"; "(+ ?a 0)"  => "?a"),
-        rewrite!("+-lft-identity"; "(+ 0 ?a)"  => "?a"),
-    ]
-}
+//         rewrite!("cancel-+-";  "(- (+ ?a ?b) ?a)" => "?b"),
+//         rewrite!("cancel-+-2"; "(- (+ ?a ?b) ?b)" => "?a"),
+
+//         rewrite!("cancel--+";  "(- ?a (+ ?a ?b))" => "(neg ?b)"),
+//         rewrite!("cancel--+2"; "(- ?a (+ ?b ?a))" => "(neg ?b)"),
+
+//         rewrite!("+-inverses";     "(- ?a ?a)" => "0"),
+//         rewrite!("+-rgt-identity"; "(+ ?a 0)"  => "?a"),
+//         rewrite!("+-lft-identity"; "(+ 0 ?a)"  => "?a"),
+//     ]
+// }
