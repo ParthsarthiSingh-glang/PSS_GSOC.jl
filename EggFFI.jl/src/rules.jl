6c26d83ba2b7db58f2f1f2cc0f885f5399464616
ParthsarthiSingh-glang <pssclass12th@gmail.com>
2026-06-30 09:53:18 +0530

    fix : adds remainder(x,y)

--- EggFFI.jl/src/rules.jl
+++ EggFFI.jl/src/rules.jl
@@ -27,6 +27,9 @@ const OP_MAP = Dict{String, Any}(
     "log" => log,
     "copysign" => Base.copysign,
 
+    # Julia's rem(x, y, RoundNearest) , will keep it in further checking
+    "remainder" => (x, y) -> Base.rem(x, y, RoundNearest),
+
     # General Math — unary
     "sqrt" => sqrt,
     "cbrt" => cbrt,
