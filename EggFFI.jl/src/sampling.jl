f0f68ae038e77bdb09f87d2a398f551f85c7338e
ParthsarthiSingh-glang <pssclass12th@gmail.com>
2026-07-06 18:30:48 +0530

    adds : concrete types and f(x,y...) case

--- EggFFI.jl/src/sampling.jl
+++ EggFFI.jl/src/sampling.jl
@@ -7,8 +7,8 @@ Holds sample points and the original expression's value at each point .
 SampleContext can be used by future calls .
 """
 struct SampleContext
-    points::Vector{Dict}
-    original_values::Vector
+    points::Vector{Dict{Num, Interval{BigFloat}}}
+    original_values::Vector{Interval{BigFloat}}
 end
 
 """
@@ -19,13 +19,15 @@ function sample_context(expr, vars;
                          range=interval(BigFloat(-10), BigFloat(10)),
                          n::Int=10)
     subintervals = mince(range, n)
-    points = Dict[]
-    values = []
+    f = Symbolics.build_function(expr, vars...; expression=Val{false}, nanmath=false)
+    points = Dict{Num, Interval{BigFloat}}[]
+    values = Interval{BigFloat}[]
     for sub in subintervals
         m = mid(sub)
-        point = Dict(v => interval(BigFloat(m)) for v in vars)
-        val = substitute(expr, point)
+        pt_vals = [interval(BigFloat(m)) for _ in vars]
+        val = f(pt_vals...)
         isempty_interval(val) && continue
+        point = Dict(v => pt_vals[i] for (i, v) in enumerate(vars))
         push!(points, point)
         push!(values, val)
     end
