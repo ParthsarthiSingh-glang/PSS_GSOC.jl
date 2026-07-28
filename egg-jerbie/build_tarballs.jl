1b1160d253fd0e7fe7b19eb04436666a1df71971
ParthsarthiSingh-glang <pssclass12th@gmail.com>
2026-07-28 15:21:53 +0530

    switched to git dependency + cp->mv

--- egg-jerbie/build_tarballs.jl
+++ egg-jerbie/build_tarballs.jl
@@ -5,14 +5,10 @@ version = v"0.1.0"
 
 sources = [
     GitSource("https://github.com/JuliaSymbolics/Jerbie.jl.git", "b1cc064252ce343830b14720ddb546e14dd1e542"),
-    GitSource("https://github.com/herbie-fp/rival3.git", "58cb38e2cbad842a024fe9ef47cd581618ca20f2"; unpack_target="rival3"),
 ]
 
 script = raw"""
 cd $WORKSPACE/srcdir/Jerbie.jl/egg-jerbie
-
-rm -rf rival3
-cp -r $WORKSPACE/srcdir/rival3 ./rival3
 
 cargo build --release
 install -Dvm 755 target/${rust_target}/release/*jerbie.${dlext} "${libdir}/libjerbie.${dlext}"
