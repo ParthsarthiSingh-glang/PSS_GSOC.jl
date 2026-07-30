dcb01ae01f8b2512c374de3a5d85ebdcae76ee68
ParthsarthiSingh-glang <pssclass12th@gmail.com>
2026-07-30 15:20:15 +0530

    add CompilerSupportLibraries_jll dependency for libgcc_s

--- egg-jerbie/build_tarballs.jl
+++ egg-jerbie/build_tarballs.jl
@@ -22,6 +22,7 @@ products = [
 ]
 
 dependencies = Dependency[
+    Dependency("CompilerSupportLibraries_jll"),
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.9", compilers=[:rust, :c])
